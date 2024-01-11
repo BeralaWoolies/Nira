17946307e2d146369f87a573ecc37fc57bafc3d8
Clarke <z5421183@ad.unsw.edu.au>
2024-01-12 04:55:41 +1100

    added projects navlink styled depending on active or not

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import UserAvatar from "@/components/UserAvatar";
 import { ThemeToggle } from "./ThemeToggle";
+import NavLinks from "@/components/NavLinks";
 
 export default function NavBar() {
   return (
     <nav className="fixed flex w-screen items-center justify-between border-b px-5 py-3 shadow-sm">
-      <div>
-        <Link href="/projects">
-          <Button variant="ghost">
-            <h1 className="text-3xl font-bold">Nira</h1>
-          </Button>
-        </Link>
+      <div className="flex items-center gap-6">
+        <NavLinks />
       </div>
       <div className="flex items-center gap-3">
         <ThemeToggle />
---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <Link href="/projects">
+        <Button variant="ghost">
+          <h1 className="text-2xl font-bold">Nira</h1>
+        </Button>
+      </Link>
+      {navLinks.map((navLink, index) => {
+        const isActive = pathname.startsWith(navLink.href);
+        return (
+          <Link key={index} href={navLink.href}>
+            <Button variant={isActive ? "secondary" : "ghost"}>
+              <h2 className={cn("text-base", isActive && "font-semibold")}>{navLink.label}</h2>
+            </Button>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
