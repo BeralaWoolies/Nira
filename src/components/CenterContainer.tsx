0601399dac0482ed3ad33f7376d08c73c39aa562
Clarke <z5421183@ad.unsw.edu.au>
2024-01-12 23:29:43 +1100

    refactor: changed min-h-screen to min-h-dvh

--- src/components/CenterContainer.tsx
+++ src/components/CenterContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function CenterContainer({ children }: { children: React.ReactNode }) {
-  return <main className="flex min-h-screen flex-col items-center p-24">{children}</main>;
+  return <main className="flex min-h-dvh flex-col items-center p-24">{children}</main>;
 }
