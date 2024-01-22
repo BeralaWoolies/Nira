92c0780ccb5fd3cd4baff5a184251d18c56c04c1
Clarke <z5421183@ad.unsw.edu.au>
2024-01-23 04:38:41 +1100

    refactor: moved ThemeProvider into providers folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import ThemeProvider from "@/components/ThemeProvider";
+import ThemeProvider from "@/providers/ThemeProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
