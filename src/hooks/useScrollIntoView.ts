6818a2436f50dc8d60ac4d538927a1903964dc50
Clarke <z5421183@ad.unsw.edu.au>
2024-01-24 03:23:43 +1100

    style && refactor: modified useScrollIntoView hook to be generic and changed scroll behaviour to smooth

--- src/hooks/useScrollIntoView.ts
+++ src/hooks/useScrollIntoView.ts
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
-export default function useScrollIntoView() {
-  return useCallback((ref: Element | null) => {
-    ref?.scrollIntoView();
+export default function useScrollIntoView<T extends Element>() {
+  return useCallback((ref: T | null) => {
+    ref?.scrollIntoView({ behavior: "smooth" });
   }, []);
 }
